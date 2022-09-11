5cbb49be333ee9b613c44e1076b14abadfc37b47
romacka <112880465+romacka@users.noreply.github.com>
2022-09-11 19:01:29 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -11,6 +11,8 @@ class Generate:
 
     def sentence(self, model):
         first_word = random.choice(list(model))
+        while model[first_word] == []:
+            first_word = random.choice(list(model))
         sentence = [first_word]
         for i in range(self.length - 1):
             temp_val = fit_model[first_word]
@@ -20,8 +22,11 @@ class Generate:
             while len(temp_val) > 2:
                 temp_val.pop(temp_prob.index(min(temp_prob)))
                 temp_prob.pop(temp_prob.index(min(temp_prob)))
-            first_word = random.choice(list(temp_val))[0]
-            sentence.append(first_word)
+            if temp_val == []:
+                break
+            else:
+                first_word = random.choice(list(temp_val))[0]
+                sentence.append(first_word)
         return ' '.join(sentence).capitalize()
 
     def load(self, model):
